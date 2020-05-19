0a57360413e429017e8b6a432680cca21826f62c
Nick Downton <nick.downton@acquia.com>
2020-05-19 15:42:17 +0100

    Initial plugin commit

---
+++ README.md
@@ -0,0 +1,95 @@
+Acquia BLT Secret management
+====
+
+This is an [Acquia BLT](https://github.com/acquia/blt) plugin providing an easy way to control [secrets storage](https://docs.acquia.com/resource/secrets/) .
+
+This plugin is **community-created** and **community-supported**. Acquia does not provide any direct support for this software or provide any warranty as to its stability.
+
+The plugin uses ansible and ansible-vault to store secrets credentials encrypted in your repository. 
+It then allows you to deploy these credentials in a secure and repeatable way, ensuring that secret settings files have correct syntax and are up to date.
+
+## Installation and usage
+
+To use this plugin, you must already have a Drupal project using BLT.
+The plugin assumes your drush aliases have ssh hostnames and usernames, and that you have SSH keys to access those environments already configured. 
+
+In your project, require the plugin with Composer:
+
+`composer require nedsbeds/blt-secrets-management`
+
+## Creating a new vault
+
+Initialize the new vault by calling `secrets:vault:init` which will prompt you for a new password to encrypt your vault.
+It will create a minimal vault file for adding your secrets.
+
+## Editing your vault
+
+Call the command `secrets:edit` which will prompt for your password to decrypt the vault file. 
+Your default editor will open with a temp file where you can make your changes. Once done, save and close the file for it to be re-enccrypted. 
+You should now commit the vault file to your repository
+
+> Note: You can change the default editor by setting the environment variable DEFAULT_EDITOR
+> 
+> e.g. `export DEFAULT_EDITOR=subl -w`
+
+
+## Diff command
+
+Call the command `secrets:diff` with a drush alias and the plugin will first create your secrets.settings.php file from your encrypted information, then run a php lint to ensure it is valid PHP. It will then show you any differences between the generated settings file and the file on that environment.  
+
+## Deploy command
+
+Call the command `secrets:deploy` with a drush alias and the plugin will first create your secrets.settings.php file from your encrypted information, then run a php lint to ensure it is valid PHP. It will then overwrite the settings file on that environment with your new values.
+
+## Adding new settings
+
+The plugin requires to elements. 
+* the vault-file with your credentials
+* A settings.secrets.php template to show how your credentials are used 
+
+The vault file is in JSON format and allows you to have a different credential per environment.
+
+```json
+{
+  "secrets": {
+    "example_api_key": {
+      "@local": "localsecret",
+      "@dev": "devsecret",
+      "@test": "stagesecret",
+      "@prod": "prodsecret"
+      }
+    }
+}
+```
+
+You should add your own credentials in the `secrets` array, and update the environment names to match your drush aliases
+
+To edit this file, run `secrets:edit`
+
+The secrets.settings.php template is located in `/secrets/secrets.settings.php.j2` and uses the jinja templating language.
+
+```php
+<?php
+
+// Example setting
+$settings['example_api_key'] = '{{secrets['example_api_key'][drush_alias]}}';
+```
+
+The jinja variable `{{secrets['example_api_key'][drush_alias]}}` will get data from your vault.
+
+Add new settings as needed.
+
+## Passwords in keychain
+
+To make dealing with the vault easier, you can run `secrets:keychain:init` which will prompt you for your vault password, add it to your keychain and then use that in future instead of prompting for your password each time.
+
+
+## Example workflow
+
+The imagined workflow for this tool is 
+
+# License
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
---
+++ src/Blt/Plugin/Commands/SecretsCommands.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace Acquia\SecretsManagement\Blt\Plugin\Commands;
+
+use Acquia\Blt\Robo\BltTasks;
+use Acquia\Blt\Robo\Exceptions\BltException;
+use Robo\Contract\VerbosityThresholdInterface;
+
+/**
+ * Defines commands in the "secrets" namespace.
+ */
+class SecretsCommands extends BltTasks {
+
+  /**
+   * The directory containing this file.
+   *
+   * @var string
+   */
+  private $pluginRoot = '/vendor/bin/nedsbeds/blt-secrets-management';
+
+  /**
+   * Initializes default secrets configuration for this project.
+   *
+   * @command secrets:vault:init
+   * @throws \Acquia\Blt\Robo\Exceptions\BltException
+   */
+  public function secretsInit() {
+
+    if ($this->confirm("<error>Are you sure you want to initialise? This will overwrite any existing encrypted vaults</error>", FALSE)) {
+
+      $result = $this->taskFilesystemStack()
+        ->copy($this->getConfigValue('repo.root') . $this->pluginRoot . '/ansible/secrets.settings.php.j2', $this->getConfigValue('repo.root') . '/secrets/secrets.settings.php.j2', TRUE)
+        ->copy($this->getConfigValue('repo.root') . $this->pluginRoot . '/ansible/.gitignore', $this->getConfigValue('repo.root') . '/secrets/.gitignore', TRUE)
+        ->stopOnFail()
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+        ->run();
+
+      if (!$result->wasSuccessful()) {
+        throw new BltException("Could not initialize secrets configuration.");
+      }
+
+      $this->taskExec("ansible-vault encrypt $this->pluginRoot/ansible/secrets_vault.yml --ask-vault-pass --output secrets/secrets_vault")->run();
+
+      $this->say("<info>A New ansible-vault and template were copied to your repository.</info>");
+      $this->say("<info>Run 'blt secrets:edit' to edit your vault.</info>");
+    }
+  }
+
+  /**
+   * Adds your vault password to the keychain.
+   *
+   * @command secrets:keychain:init
+   */
+  public function vaultPasswordFileInit() {
+    $vaultID = $this->getVaultId();
+    $this->taskExec('security add-generic-password -a ' . $vaultID . ' -s "' . $vaultID . ' Password" -w')->run();
+
+    $this->taskFilesystemStack()
+      ->touch($this->getConfigValue('repo.root') . '/secrets/.usekeychain')
+      ->run();
+  }
+
+  /**
+   * Get the Password from keychain for decrypting the vault.
+   *
+   * @command secrets:vault:password
+   */
+  public function vaultPasswordFile() {
+    $vaultID = $this->getVaultId();
+    $this->taskExec(
+      "/usr/bin/security find-generic-password -w \
+        -a \"$vaultID\" -l \"$vaultID Password\" 2> /dev/null"
+    )->run();
+  }
+
+  /**
+   * Edit the vault file.
+   *
+   * @command secrets:edit
+   * @aliases seed
+   * @throws \Acquia\Blt\Robo\Exceptions\BltException
+   */
+  public function secretsEdit() {
+    $command = "ansible-vault edit secrets/secrets_vault" . $this->getVaultPasswordCommand();
+    $this->taskExec($command)->run();
+  }
+
+  /**
+   * Provide a diff on your vault for a particular environment.
+   *
+   * @command secrets:diff
+   * @aliases sedf
+   * @throws \Robo\Exception\TaskException
+   */
+  public function secretsDiff($drushAlias) {
+    $environments = $this->getEnvironments();
+
+    if (isset($environments[$drushAlias])) {
+      $ansibleCommand = $this->getPlaybookCommand(
+        $drushAlias,
+        $environments,
+        "-CD"
+      );
+
+      $this->taskExec($ansibleCommand)
+        ->run();
+    }
+  }
+
+  /**
+   * Deploy the secrets in a particular environment.
+   *
+   * @param string $drushAlias
+   *   The drush alias for the environment.
+   *
+   * @command secrets:deploy
+   * @aliases sedp
+   * @throws \Robo\Exception\TaskException
+   */
+  public function secretsDeploy($drushAlias) {
+    $environments = $this->getEnvironments();
+
+    if (isset($environments[$drushAlias])) {
+      $ansibleCommand = $this->getPlaybookCommand($drushAlias, $environments);
+      $this->taskExec($ansibleCommand)->run();
+    }
+  }
+
+  /**
+   * Generate an ansible parameter for getting password from keychain.
+   *
+   * @return string
+   *   Return the vault-password-file parameter.
+   */
+  private function getVaultPasswordCommand() {
+    if (file_exists($this->getConfigValue('repo.root') . '/secrets/.usekeychain')) {
+      return ' --vault-password-file=' . $this->getConfigValue('repo.root') . $this->pluginRoot . '/ansible/vault_password_file';
+    } else {
+      return ' --ask-vault-pass';
+    }
+  }
+
+  /**
+   * Get a vault ID for naming keychain entry.
+   *
+   * @return string
+   *   The vault id used to define keychain entry name.
+   */
+  private function getVaultId() {
+    return $this->getConfigValue('project.machine_name') . '-ansible-vault';
+  }
+
+  /**
+   * Retrieve the drush aliases defined.
+   *
+   * @return array
+   *   The environments defined in drush alias file.
+   *
+   * @throws \Robo\Exception\TaskException
+   */
+  private function getEnvironments() {
+    return unserialize(
+      $this->taskDrush()
+        ->Drush("sa --format=php")
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+        ->run()
+        ->getMessage()
+    );
+  }
+
+  /**
+   * Get the ansible command for running the main playbook.
+   *
+   * @param string $drushAlias
+   *   The Drush alias.
+   * @param array $environments
+   *   Full list of environments from SecurityCommands::getEnvironments().
+   * @param string $arguments
+   *   Any extra arguments to ansible-playbook command.
+   *
+   * @return string
+   *   Full command for running the ansible playbook.
+   */
+  private function getPlaybookCommand($drushAlias, array $environments, $arguments = "") {
+    $defaultPlaybook = $this->getConfigValue('repo.root') . $this->pluginRoot . "/ansible/deploy-secrets.yml";
+    $playbook = $this->getConfigValue('secrets.playbook', $defaultPlaybook);
+
+    $secret_vault_location = $this->getConfigValue('repo.root') . "/secrets/secrets_vault";
+    $secret_template_location = $this->getConfigValue('repo.root') . "/secrets/secrets.settings.php.j2";
+    $secret_location = '/mnt/files/' . $environments[$drushAlias]['ac-site'] . '.' . $environments[$drushAlias]['ac-env'] . '/secrets.settings.php';
+
+    $command = "ansible-playbook " . $arguments . " " . $playbook .
+      " -i " . $environments[$drushAlias]['host'] . ", -u " . $environments[$drushAlias]['user'] . " \
+      --extra-vars 'drush_alias=" . $drushAlias . " \
+      secret_vault_location=" . $secret_vault_location . " \
+      secret_template_location=" . $secret_template_location . " \
+      secret_location=" . $secret_location . "' "
+      . $this->getVaultPasswordCommand();
+
+    return $command;
+  }
+
+}
